cd2042851f026f97a00dce18ce3f064c034fc149
rewin <rewin1996@gmail.com>
2024-07-21 21:01:00 +0300

    resources v2

--- src/game/resources.rs
+++ src/game/resources.rs
@@ -12,44 +12,73 @@ pub(crate) fn plugin(app: &mut App) {
     app.init_resource::<CarbonInAir>();
     app.init_resource::<MetalTrash>();
     app.init_resource::<Metal>();
+    app.init_resource::<Temperature>();
 
     #[cfg(feature = "dev")]
     app.add_plugins(dev::plugin);
 }
 
 #[derive(Resource, Default)]
-pub struct Metal(pub f32);
+pub struct Metal {
+    pub amount: f32,
+}
 
 #[derive(Resource, Default)]
-pub struct MetalTrash(pub f32);
+pub struct MetalTrash {
+    pub amount: f32,
+}
 
 #[derive(Resource, Default)]
-pub struct Water(pub f32);
+pub struct Water {
+    pub amount: f32,
+    pub limit: f32
+}
 
 #[derive(Resource, Default)]
-pub struct BadWater(pub f32);
+pub struct BadWater {
+    pub amount: f32,
+    pub limit: f32
+}
 
 /// Oxygen in ship
 #[derive(Resource, Default)]
-pub struct Oxygen(pub f32);
+pub struct Oxygen {
+    pub amount: f32,
+    pub limit: f32
+}
 
 
 /// Oxygen in ship air
 #[derive(Resource, Default)]
-pub struct OxygenInAir(pub f32); //in percent
+pub struct OxygenInAir {
+    pub amount: f32,
+    pub limit: f32
+}
 
 /// How many carbon is in the air. If its too many, then you will die
 #[derive(Resource, Default)]
-pub struct CarbonInAir(pub f32);
+pub struct CarbonInAir {
+    pub amount: f32,
+    pub limit: f32
+}
 
 #[derive(Resource, Default)]
-pub struct Pee(pub f32);
+pub struct Pee {
+    pub amount: f32,
+    pub limit: f32
+}
 
 #[derive(Resource, Default)]
-pub struct Food(pub f32);
+pub struct Food {
+    pub amount: f32,
+    pub limit: f32
+}
 
 #[derive(Resource, Default)]
-pub struct Hydrogen(pub f32);
+pub struct Hydrogen {
+    pub amount: f32,
+    pub limit: f32
+}
 
 #[derive(Resource, Default)]
 pub struct Electricity {
@@ -57,6 +86,10 @@ pub struct Electricity {
     pub used : f32 //how many electricity was used in last tick
 }
 
+#[derive(Resource, Default)]
+pub struct Temperature {
+    pub amount: f32,
+}
 
 #[cfg(feature = "dev")]
 mod dev {
@@ -82,18 +115,20 @@ mod dev {
         hydrogen: Res<Hydrogen>,
         electricity: Res<Electricity>,
         carbon_in_air: Res<CarbonInAir>,
+        temperature: Res<Temperature>,
     ) {
 
-        debug_panel.add("Metal", format!("Metal: {}", metal.0));
-        debug_panel.add("Metal trash", format!("Metal trash: {}", metal_trash.0));
-        debug_panel.add("Water", format!("Water: {}", water.0));
-        debug_panel.add("Bad water", format!("Bad water: {}", bad_water.0));
-        debug_panel.add("Oxygen", format!("Oxygen: {}", oxygen.0));
-        debug_panel.add("Oxygen in air", format!("Oxygen in air: {}", oxygen_in_air.0));
-        debug_panel.add("Pee", format!("Pee: {}", pee.0));
-        debug_panel.add("Food", format!("Food: {}", food.0));
-        debug_panel.add("Hydrogen", format!("Hydrogen: {}", hydrogen.0));
-        debug_panel.add("Electricity", format!("Electricity: {}", electricity.total - electricity.used));
-        debug_panel.add("Carbon in air", format!("Carbon in air: {}", carbon_in_air.0));
+        debug_panel.add("Metal", format!("Metal: {}", metal.amount));
+        debug_panel.add("Metal trash", format!("Metal trash: {}", metal_trash.amount));
+        debug_panel.add("Water", format!("Water: {}/{}", water.amount, water.limit));
+        debug_panel.add("Bad water", format!("Bad water: {}/{}", bad_water.amount, bad_water.limit));
+        debug_panel.add("Oxygen", format!("Oxygen: {}/{}", oxygen.amount, oxygen.limit));
+        debug_panel.add("Oxygen in air", format!("Oxygen in air: {}/{}", oxygen_in_air.amount, oxygen_in_air.limit));
+        debug_panel.add("Pee", format!("Pee: {}/{}", pee.amount, pee.limit));
+        debug_panel.add("Food", format!("Food: {}/{}", food.amount, food.limit));
+        debug_panel.add("Hydrogen", format!("Hydrogen: {}/{}", hydrogen.amount, hydrogen.limit));
+        debug_panel.add("Electricity", format!("Electricity: {}/{}", electricity.total, electricity.used));
+        debug_panel.add("Carbon in air", format!("Carbon in air: {}/{}", carbon_in_air.amount, carbon_in_air.limit));
+        debug_panel.add("Temperature", format!("Temperature: {} ℃", temperature.amount));
     }
 }
