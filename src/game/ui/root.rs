1370745ccbc177bc75f591a1f6da702cbc06c50f
nickydonna <nickydonna@fastmail.com>
2024-07-23 19:12:42 -0300

    add end scree

--- src/game/ui/root.rs
+++ src/game/ui/root.rs
@@ -2,10 +2,16 @@ use bevy::prelude::*;
 use bevy_mod_stylebuilder::{StyleBuilder, StyleBuilderBackground, StyleBuilderLayout};
 use bevy_quill::*;
 
-use crate::game::resources::{CarbonDioxide, Food, Oxygen, OxygenRecycling, Water};
+use crate::game::{
+    daycycle::PlayerState,
+    resources::{CarbonDioxide, Food, Oxygen, OxygenRecycling, Water},
+};
 
 use super::{
-    components::resource_slider::ResourceSlider,
+    components::{
+        end_screen::{EndScreen, EndType},
+        resource_slider::ResourceSlider,
+    },
     constants::{RESOURCE_MENU_PADDING, RESOURCE_MENU_WIDTH},
     context_menu, SelectedItem,
 };
@@ -27,6 +33,7 @@ fn root_style(sb: &mut StyleBuilder) {
 impl ViewTemplate for RootUi {
     type View = impl View;
     fn create(&self, cx: &mut Cx) -> Self::View {
+        let player_state = cx.use_resource::<State<PlayerState>>();
         let selected_item = cx.use_resource::<SelectedItem>();
         let oxygen = cx.use_resource::<Oxygen>();
         let oxygen_recycling = cx.use_resource::<OxygenRecycling>();
@@ -45,41 +52,70 @@ impl ViewTemplate for RootUi {
         } else {
             co2.generation_rate
         };
-        Element::<NodeBundle>::new().style(root_style).children((
-            Element::<NodeBundle>::new()
-                .style(|sb: &mut StyleBuilder| {
-                    sb.display(Display::Flex)
-                        .flex_direction(FlexDirection::Column)
-                        .top(0)
-                        .left(0)
-                        .right(0)
-                        .padding(RESOURCE_MENU_PADDING)
-                        .row_gap(15)
-                        .height(Val::Percent(100.0))
-                        .width(RESOURCE_MENU_WIDTH)
-                        .background_color(Srgba::new(1.0, 1.0, 1.0, 0.3));
-                })
-                .children((
-                    ResourceSlider::new()
-                        .limit(oxygen.limit)
-                        .amount(oxygen.amount)
-                        .label(format!("Oxygen ({:+})", oxygen_status)),
-                    ResourceSlider::new()
-                        .limit(co2.limit)
-                        .amount(co2.amount)
-                        .label(format!("CO2 ({:+})", co2_status)),
-                    ResourceSlider::new()
-                        .limit(water.limit)
-                        .amount(water.amount)
-                        .label("Water"),
-                    ResourceSlider::new()
-                        .limit(food.limit)
-                        .amount(food.amount)
-                        .label("Food"),
-                )),
-            // If the position of the menu is `Some` we show the Context Menu
-            // Other wise we show nothing
-            Cond::new(position.is_some(), context_menu::ContextMenu, ()),
-        ))
+
+        let game_ended = *player_state != PlayerState::Alive;
+
+        Element::<NodeBundle>::new()
+            .style(root_style)
+            .style_dyn(
+                |ended, sb| {
+                    if ended {
+                        sb.width(Val::Percent(100.0));
+                    } else {
+                        sb.width(Val::Auto);
+                    }
+                },
+                game_ended,
+            )
+            .children(Cond::new(
+                game_ended,
+                EndScreen::new()
+                    .end_type(if *player_state == PlayerState::Dead {
+                        EndType::Lose
+                    } else {
+                        EndType::Win
+                    })
+                    .text(if *player_state == PlayerState::Dead {
+                        "You died. Capitalism Won"
+                    } else {
+                        "You won. Now you live in space debt free. Alone. Cold. But with Money"
+                    }),
+                (
+                    Element::<NodeBundle>::new()
+                        .style(|sb: &mut StyleBuilder| {
+                            sb.display(Display::Flex)
+                                .flex_direction(FlexDirection::Column)
+                                .top(0)
+                                .left(0)
+                                .right(0)
+                                .padding(RESOURCE_MENU_PADDING)
+                                .row_gap(15)
+                                .height(Val::Percent(100.0))
+                                .width(RESOURCE_MENU_WIDTH)
+                                .background_color(Srgba::new(1.0, 1.0, 1.0, 0.3));
+                        })
+                        .children((
+                            ResourceSlider::new()
+                                .limit(oxygen.limit)
+                                .amount(oxygen.amount)
+                                .label(format!("Oxygen ({:+})", oxygen_status)),
+                            ResourceSlider::new()
+                                .limit(co2.limit)
+                                .amount(co2.amount)
+                                .label(format!("CO2 ({:+})", co2_status)),
+                            ResourceSlider::new()
+                                .limit(water.limit)
+                                .amount(water.amount)
+                                .label("Water"),
+                            ResourceSlider::new()
+                                .limit(food.limit)
+                                .amount(food.amount)
+                                .label("Food"),
+                        )),
+                    // If the position of the menu is `Some` we show the Context Menu
+                    // Other wise we show nothing
+                    Cond::new(position.is_some(), context_menu::ContextMenu, ()),
+                ),
+            ))
     }
 }
