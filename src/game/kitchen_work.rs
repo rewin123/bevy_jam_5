fb3fb4bf2c8fcc30d8c472e2f28b9df5a437dd5b
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-07-25 22:54:44 -0400

    start to complete cooking food

--- src/game/kitchen_work.rs
+++ src/game/kitchen_work.rs
@@ -1,7 +1,15 @@
 use bevy::prelude::*;
+use bevy_mod_billboard::BillboardTextBundle;
+
+use crate::game::{
+    components::flowup_text::FlowUpText, sequence::NextAction, spawn::spawn_commands::SpawnKitchen,
+};
 
 use super::{
-    character::CharacterStates, components::kitchen::Kitchen, daycycle::GameTime, resources::Food,
+    character::{CharState, CharacterStates},
+    components::kitchen::Kitchen,
+    daycycle::GameTime,
+    resources::Food,
     sequence::CharacterAction,
 };
 
@@ -48,5 +56,33 @@ pub fn update_work_in_kitchen(
     mut food: ResMut<Food>,
     q_kitchen: Query<&GlobalTransform, With<Kitchen>>,
 ) {
-    // todo : implement the update on the work done in the kitchen
+    for (entity, mut kitchen_work, mut states) in q_kitchen_work.iter_mut() {
+        states.add(CharState::Working);
+
+        kitchen_work.work_time += time.delta_seconds();
+        if kitchen_work.work_time > kitchen_work_config.work_time {
+            // todo : Add poop so that you need to go to the toilet again. Recycle so that you can produce food
+            // todo: complete so that you can change ressource
+
+            info!("Doing Kitchen Work");
+            commands.entity(entity).remove::<KitchenWork>();
+            commands.trigger_targets(NextAction, entity);
+
+            if let Ok(pc_transform) = q_kitchen.get_single() {
+                let text_style = TextStyle {
+                    color: Color::linear_rgb(0.0, 1.0, 0.0),
+                    font_size: 94.0,
+                    ..default()
+                };
+                commands
+                    .spawn(BillboardTextBundle {
+                        transform: Transform::from_translation(pc_transform.translation())
+                            .with_scale(Vec3::splat(0.01)),
+                        text: Text::from_section(format!("Making Food"), text_style),
+                        ..default()
+                    })
+                    .insert(FlowUpText { lifetime: 1.0 });
+            }
+        }
+    }
 }
